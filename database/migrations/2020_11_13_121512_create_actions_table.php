df30af31d4f91f49d655b7486fa54ee6e2e1971a
mlab817 <mlab817@gmail.com>
2020-11-13 21:26:36 +0800

    fixed migrations

--- database/migrations/2020_11_13_121512_create_actions_table.php
+++ database/migrations/2020_11_13_121512_create_actions_table.php
@@ -15,7 +15,7 @@ class CreateActionsTable extends Migration
     {
         Schema::create('actions', function (Blueprint $table) {
             $table->id();
-            $ttable->string('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
